977a17d25ebfe1a179d75a928e2b584f72f15420
Mcalmic <69126774+Mcalmic@users.noreply.github.com>
2021-01-25 20:25:09 -0800

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,24 @@
+class Box{
+  constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.isVisible = true;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed < 3){
+        rectMode(CENTER);
+        if(this.isVisible === true){
+          rect(this.body.position.x, this.body.position.y, this.width, this.height);
+        }
+      } else {
+        this.isVisible = false;
+      } 
+    }
+}
